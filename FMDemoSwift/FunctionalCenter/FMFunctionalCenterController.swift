39751bcb9cccf8cdd8a3b2074e6b175f356dbb42
xiwang wang <xiwang.wang@palmaplus.comxiwang.wang@palmaplus.comxiwang.wang@palmaplus.comxiwang.wang@palmaplus.comxiwang.wang@palmaplus.comxiwang.wang@palmaplus.com>
2018-11-26 17:51:49 +0800

    sth

---
+++ FMDemoSwift/FunctionalCenter/FMFunctionalCenterController.swift
@@ -0,0 +1,35 @@
+//
+//  FMFunctionalCenterController.swift
+//  FMDemoSwift
+//
+//  Created by xiwang wang on 2018/8/30.
+//  Copyright © 2018年 com.simon. All rights reserved.
+//
+
+import UIKit
+
+class FMFunctionalCenterController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
